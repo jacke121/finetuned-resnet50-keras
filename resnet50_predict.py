e820aa84d0121635f2673d8b4c2364afd18949cb
jacke121 <jacke121@126.com>
2017-12-28 20:14:14 +0800

    Update resnet50_predict.py

--- resnet50_predict.py
+++ resnet50_predict.py
@@ -7,22 +7,37 @@ from keras.applications.imagenet_utils import preprocess_input
 from keras.models import load_model
 from keras.preprocessing import image
 
+class ResNetP:
+    def __init__(self,model_path):
+        t0 = time.time()
+        self.model = load_model(model_path)
+        t1 = time.time()
+        print('Loaded in:', t1 - t0)
 
-def predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    preds = model.predict(x)
-    return preds
+    def predict(self,img_path):
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        preds = self.model.predict(x)
+        return preds
 
 if __name__ == '__main__':
-    model_path = sys.argv[1]
-    print('Loading model:', model_path)
-    t0 = time.time()
-    model = load_model(model_path)
-    t1 = time.time()
-    print('Loaded in:', t1-t0)
+    model_path = "resnet50_best.h5"
+    # print('Loading model:', model_path)
+    resnet=ResNetP(model_path)
+    path = '../white_mouse/test/1/'
+    files = os.listdir(path)
 
-    test_path = sys.argv[2]
-    print('Generating predictions on image:', sys.argv[2])
-    preds = predict(sys.argv[2], model)
+    aaa=0
+    index=0
+    for file in files:
+    # print('Generating predictions on image:', sys.argv[2])
+        t0 = time.time()
+        preds = resnet.predict(path + file)
+        t1 = time.time()
+        print('Loaded in:', t1 - t0)
+        result=np.argmax(preds,1)[0]
+        aaa+=result
+        print(result)
+        index+=1
+    print(aaa,index)
